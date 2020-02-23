176a5a1b5b0bee749fd1f50f5f8fecf3dcc20ddd
BBuf <1182563586@qq.com>
2020-02-23 22:26:42 +0800

    Add Init 2020/2/23

--- src/batchnorm_layer.c
+++ src/batchnorm_layer.c
@@ -3,41 +3,42 @@
 #include "utils.h"
 #include <stdio.h>
 
+
 layer make_batchnorm_layer(int batch, int w, int h, int c, int train)
 {
     fprintf(stderr, "Batch Normalization Layer: %d x %d x %d image\n", w,h,c);
-    layer layer = { (LAYER_TYPE)0 };
-    layer.type = BATCHNORM;
-    layer.batch = batch;
-    layer.train = train;
-    layer.h = layer.out_h = h;
-    layer.w = layer.out_w = w;
-    layer.c = layer.out_c = c;
+    layer layer = { (LAYER_TYPE)0 }; 
+    layer.type = BATCHNORM; // 网络层的名字
+    layer.batch = batch; //一个batch中包含的图片数
+    layer.train = train; 
+    layer.h = layer.out_h = h;  // 当前层的输出高度等于输入高度h
+    layer.w = layer.out_w = w; // 当前层的输出宽度等于输入宽度w
+    layer.c = layer.out_c = c; // 当前层的输出通道数等于输入通道数
 
     layer.n = layer.c;
-    layer.output = (float*)xcalloc(h * w * c * batch, sizeof(float));
-    layer.delta = (float*)xcalloc(h * w * c * batch, sizeof(float));
-    layer.inputs = w*h*c;
-    layer.outputs = layer.inputs;
+    layer.output = (float*)xcalloc(h * w * c * batch, sizeof(float)); // layer.output为该层所有的输出（包括mini-batch所有输入图片的输出）
+    layer.delta = (float*)xcalloc(h * w * c * batch, sizeof(float)); //layer.delta 是该层的敏感度图，和输出的维度想同
+    layer.inputs = w*h*c; //mini-batch中每张输入图片的像素元素个数
+    layer.outputs = layer.inputs; // 对应每张输入图片的所有输出特征图的总元素个数（每张输入图片会得到n也即layer.out_c张特征图）
 
-    layer.biases = (float*)xcalloc(c, sizeof(float));
-    layer.bias_updates = (float*)xcalloc(c, sizeof(float));
+    layer.biases = (float*)xcalloc(c, sizeof(float)); // BN层特有参数，缩放系数
+    layer.bias_updates = (float*)xcalloc(c, sizeof(float)); // 缩放系数的敏感度图
 
-    layer.scales = (float*)xcalloc(c, sizeof(float));
-    layer.scale_updates = (float*)xcalloc(c, sizeof(float));
+    layer.scales = (float*)xcalloc(c, sizeof(float)); // BN层特有参数，偏置系数
+    layer.scale_updates = (float*)xcalloc(c, sizeof(float)); // 偏置系数的敏感度图
     int i;
     for(i = 0; i < c; ++i){
-        layer.scales[i] = 1;
+        layer.scales[i] = 1; // 将缩放系数初始化为1
     }
+ 
+    layer.mean = (float*)xcalloc(c, sizeof(float)); // mean 一个batch中所有图片的均值，分通道求取
+    layer.variance = (float*)xcalloc(c, sizeof(float));  // variance 一个batch中所有图片的方差，分通道求取
 
-    layer.mean = (float*)xcalloc(c, sizeof(float));
-    layer.variance = (float*)xcalloc(c, sizeof(float));
+    layer.rolling_mean = (float*)xcalloc(c, sizeof(float)); // 均值的滑动平均，影子变量
+    layer.rolling_variance = (float*)xcalloc(c, sizeof(float)); // 方差的滑动平均，影子变量
 
-    layer.rolling_mean = (float*)xcalloc(c, sizeof(float));
-    layer.rolling_variance = (float*)xcalloc(c, sizeof(float));
-
-    layer.forward = forward_batchnorm_layer;
-    layer.backward = backward_batchnorm_layer;
+    layer.forward = forward_batchnorm_layer; // 前向传播函数
+    layer.backward = backward_batchnorm_layer; // 反向传播函数
     layer.update = update_batchnorm_layer;
 #ifdef GPU
     layer.forward_gpu = forward_batchnorm_layer_gpu;
@@ -82,6 +83,16 @@ layer make_batchnorm_layer(int batch, int w, int h, int c, int train)
     return layer;
 }
 
+// 求gamma的梯度,对应公式 BN 2-6
+//backward_scale_cpu(l.x_norm, l.delta, l.batch, l.out_c, l.out_w*l.out_h, l.scale_updates);
+// x_norm 代表BN层前向传播的输出值
+// delta 代表上一层的梯度图
+// batch 为l.batch，即一个batch的图片数
+// n代表输出通道数，也即是输入通道数
+// size 代表w * h
+// scale_updates 代表scale的梯度更新值
+// y = gamma * x + beta
+// dy / d(gamma) = x
 void backward_scale_cpu(float *x_norm, float *delta, int batch, int n, int size, float *scale_updates)
 {
     int i,b,f;
@@ -97,6 +108,10 @@ void backward_scale_cpu(float *x_norm, float *delta, int batch, int n, int size,
     }
 }
 
+// 求y对均值的导数,对应公式 BN 2-2
+//不过Darknet特殊的点在于是先计算均值的梯度
+// 这个时候方差是没有梯度的，所以公式3的后半部分为0，也就只保留了公式3的前半部分
+// 不过我从理论上无法解释这种操作会带来什么影响，但从目标检测来看应该是没有影响的
 void mean_delta_cpu(float *delta, float *variance, int batch, int filters, int spatial, float *mean_delta)
 {
 
@@ -112,6 +127,8 @@ void mean_delta_cpu(float *delta, float *variance, int batch, int filters, int s
         mean_delta[i] *= (-1./sqrt(variance[i] + .00001f));
     }
 }
+
+// 求y对方差的导数,对应公式 BN 2-1
 void  variance_delta_cpu(float *x, float *delta, float *mean, float *variance, int batch, int filters, int spatial, float *variance_delta)
 {
 
@@ -127,6 +144,9 @@ void  variance_delta_cpu(float *x, float *delta, float *mean, float *variance, i
         variance_delta[i] *= -.5 * pow(variance[i] + .00001f, (float)(-3./2.));
     }
 }
+
+// 求出BN层的梯度敏感度图
+// 对应了论文中的求导公式BN 1-6，即是对x_i求导
 void normalize_delta_cpu(float *x, float *mean, float *variance, float *mean_delta, float *variance_delta, int batch, int filters, int spatial, float *delta)
 {
     int f, j, k;
@@ -176,6 +196,7 @@ void resize_batchnorm_layer(layer *l, int w, int h)
 #endif // GPU
 }
 
+// BN层的前向传播函数
 void forward_batchnorm_layer(layer l, network_state state)
 {
     if(l.type == BATCHNORM) copy_cpu(l.outputs*l.batch, state.input, 1, l.output, 1);
@@ -183,34 +204,47 @@ void forward_batchnorm_layer(layer l, network_state state)
         l.out_c = l.outputs;
         l.out_h = l.out_w = 1;
     }
+	// 训练阶段
     if(state.train){
+		// blas.c中有详细注释，计算输入数据的均值，保存为l.mean
         mean_cpu(l.output, l.batch, l.out_c, l.out_h*l.out_w, l.mean);
+		// blas.c中有详细注释，计算输入数据的方差，保存为l.variance
         variance_cpu(l.output, l.mean, l.batch, l.out_c, l.out_h*l.out_w, l.variance);
 
+		// 计算滑动平均和方差，影子变量
         scal_cpu(l.out_c, .9, l.rolling_mean, 1);
         axpy_cpu(l.out_c, .1, l.mean, 1, l.rolling_mean, 1);
         scal_cpu(l.out_c, .9, l.rolling_variance, 1);
         axpy_cpu(l.out_c, .1, l.variance, 1, l.rolling_variance, 1);
 
         copy_cpu(l.outputs*l.batch, l.output, 1, l.x, 1);
+		// 减去均值，除以方差得到x^，论文中的第3个公式
         normalize_cpu(l.output, l.mean, l.variance, l.batch, l.out_c, l.out_h*l.out_w);
+		// BN层的输出
         copy_cpu(l.outputs*l.batch, l.output, 1, l.x_norm, 1);
     } else {
+		// 测试阶段，直接用滑动变量来计算输出
         normalize_cpu(l.output, l.rolling_mean, l.rolling_variance, l.batch, l.out_c, l.out_h*l.out_w);
     }
+	// 最后一个公式，对输出进行移位和偏置
     scale_bias(l.output, l.scales, l.batch, l.out_c, l.out_h*l.out_w);
     add_bias(l.output, l.biases, l.batch, l.out_c, l.out_w*l.out_h);
 }
 
+// BN层的反向传播函数
 void backward_batchnorm_layer(const layer l, network_state state)
 {
+	// 这里是对论文中最后一个公式的缩放系数求梯度更新值
     backward_scale_cpu(l.x_norm, l.delta, l.batch, l.out_c, l.out_w*l.out_h, l.scale_updates);
-
+	// 也是在convlution_layer.c中定义的函数，先将敏感度图乘以l.scales
     scale_bias(l.delta, l.scales, l.batch, l.out_c, l.out_h*l.out_w);
-
+	
+	//对均值求倒数
     mean_delta_cpu(l.delta, l.variance, l.batch, l.out_c, l.out_w*l.out_h, l.mean_delta);
-    variance_delta_cpu(l.x, l.delta, l.mean, l.variance, l.batch, l.out_c, l.out_w*l.out_h, l.variance_delta);
-    normalize_delta_cpu(l.x, l.mean, l.variance, l.mean_delta, l.variance_delta, l.batch, l.out_c, l.out_w*l.out_h, l.delta);
+    //对方差求倒数
+	variance_delta_cpu(l.x, l.delta, l.mean, l.variance, l.batch, l.out_c, l.out_w*l.out_h, l.variance_delta);
+    //计算敏感度图，对应了论文中的最后一部分
+	normalize_delta_cpu(l.x, l.mean, l.variance, l.mean_delta, l.variance_delta, l.batch, l.out_c, l.out_w*l.out_h, l.delta);
     if(l.type == BATCHNORM) copy_cpu(l.outputs*l.batch, l.delta, 1, state.delta, 1);
 }
 
